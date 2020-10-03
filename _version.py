3795d87a515a9d132a7fbe62a16923f6071c3f01
Zhou Xuebin <qhgz2011@hotmail.com>
2020-10-03 21:56:39 +0800

    feature update: support all apple type

--- _version.py
+++ _version.py
@@ -4,14 +4,14 @@ __all__ = ['VERSION']
 def version_wrapper() -> str:
     from util import spawn_process
     git_tag = spawn_process('git tag --points-at HEAD')[1]
+    worktree_flag = spawn_process('git diff-index --quiet HEAD')[0]
+    worktree_status = '-dirty' if worktree_flag != 0 else ''
     if len(git_tag) > 0:
-        return git_tag
+        return git_tag + worktree_status
     else:
         git_branch = spawn_process('git rev-parse --abbrev-ref HEAD')[1]
         git_hash = spawn_process('git rev-parse --short HEAD')[1]
         git_time = spawn_process('git show -s --format=%cI HEAD')[1]
-        worktree_flag = spawn_process('git diff-index --quiet HEAD')[0]
-        worktree_status = '-dirty' if worktree_flag != 0 else ''
         return f'{git_branch}-{git_hash}-{git_time}{worktree_status}'
 
 
--- fsm/eat_apple_handler.py
+++ fsm/eat_apple_handler.py
@@ -18,6 +18,9 @@ def _imread_to_screen_size(path):
 
 class EatAppleHandler(ConfigurableStateHandler):
     _eat_apple_ui_anchor = _imread_to_screen_size(CV_EAT_APPLE_UI_FILE)
+    _y_mapper = {EatAppleType.GoldApple: EAT_GOLD_APPLE_CLICK_Y, EatAppleType.SilverApple: EAT_SILVER_APPLE_CLICK_Y,
+                 EatAppleType.BronzeApple: EAT_BRONZE_APPLE_CLICK_Y, EatAppleType.SaintQuartz: EAT_SAINT_QUARTZ_CLICK_Y}
+    __warned_eat_saint_quartz = False
 
     def __init__(self, attacher: AbstractAttacher, forward_state: FgoState, cfg: ScriptConfiguration):
         super().__init__(cfg)
@@ -27,23 +30,21 @@ class EatAppleHandler(ConfigurableStateHandler):
     def run_and_transit_state(self) -> FgoState:
         screenshot = self.attacher.get_screenshot(CV_SCREENSHOT_RESOLUTION_X, CV_SCREENSHOT_RESOLUTION_Y)
         if self.is_in_eat_apple_ui(screenshot):
-            if self._cfg.eat_apple_type == EatAppleType.GoldApple:
-                # TODO: validate this behavior
-                logger.info('Performing action: eat apple')
-                self.attacher.send_click(EAT_GOLD_APPLE_CLICK_X, EAT_GOLD_APPLE_CLICK_Y)
-                sleep(0.5)
-                self.attacher.send_click(EAT_APPLE_CONFIRM_CLICK_X, EAT_APPLE_CONFIRM_CLICK_Y)
-                sleep(0.5)
-                # WaitFufuStateHandler(self.attacher, 0).run_and_transit_state()
-                return WaitFufuStateHandler(self.attacher, self.forward_state).run_and_transit_state()
-            elif self._cfg.eat_apple_type == EatAppleType.DontEatMyApple:
+            if self._cfg.eat_apple_type == EatAppleType.DontEatMyApple:
                 logger.warning('AP is not enough to enter quest, exit')
                 self.attacher.send_click(CANCEL_EAT_APPLE_BUTTON_X, CANCEL_EAT_APPLE_BUTTON_Y)
                 sleep(0.5)
                 return FgoState.STATE_FINISH
             else:
-                logger.critical('Not implemented EatAppleType: %s' % str(self._cfg.eat_apple_type))
-                return FgoState.STATE_ERROR
+                if self._cfg.eat_apple_type == EatAppleType.SaintQuartz and not self.__warned_eat_saint_quartz:
+                    self.__warned_eat_saint_quartz = True
+                    logger.warning('You are using saint quartz for ap recovery')
+                logger.info('Performing action: ap recovery')
+                self.attacher.send_click(EAT_APPLE_CLICK_X, self._y_mapper[self._cfg.eat_apple_type])
+                sleep(0.5)
+                self.attacher.send_click(EAT_APPLE_CONFIRM_CLICK_X, EAT_APPLE_CONFIRM_CLICK_Y)
+                sleep(0.5)
+                return WaitFufuStateHandler(self.attacher, self.forward_state).run_and_transit_state()
         else:
             logger.debug('Eat apple scene not presented, operation skipped')
             return self.forward_state
